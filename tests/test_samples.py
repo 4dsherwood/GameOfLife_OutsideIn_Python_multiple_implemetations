b4c2798ac436f699460839f354ea1824c029949b
4dsherwood <4dsherwood@users.noreply.github.com>
2023-11-07 15:39:40 -0700

    fully working one

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -12,7 +12,10 @@ class Game:
         self.oneone = DEAD
 
     def get_status(self, x, y):
-        return self.oneone
+        if (x==1 and y ==1):
+            return self.oneone
+        else:
+            return DEAD
 
 
 def create_game_of_life_with_all_dead_cell():
@@ -24,6 +27,7 @@ def test_given_living_cell_with_0_live_nieghbors_it_dies():
     game = create_game_of_life_with_all_dead_cell()
     # set (1,1) cell to alive
     game.set_cell_alive(1,1)
+    assert game.get_status(0, 0) == DEAD
     assert game.get_status(1, 1) == ALIVE
     # move to the next point in time
     game.move_to_next_time()

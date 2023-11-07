c24f99103254e9bf278197c523aae17f684d281c
4dsherwood <4dsherwood@users.noreply.github.com>
2023-11-07 15:57:00 -0700

    using matrix

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -4,18 +4,19 @@ DEAD = 0
 ALIVE = 1
 class Game:
     def __init__(self):
-        self.oneone = DEAD
+        self.matrix = [[0, 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]]
     def set_cell_alive(self, x, y):
-        self.oneone = ALIVE
+        self.matrix[x][y] = ALIVE
 
     def move_to_next_time(self):
-        self.oneone = DEAD
+        self.matrix =  [[0, 0, 0],
+                        [0, 0, 0],
+                        [0, 0, 0]]
 
     def get_status(self, x, y):
-        if (x==1 and y ==1):
-            return self.oneone
-        else:
-            return DEAD
+        return self.matrix[x][y]
 
 
 def create_game_of_life_with_all_dead_cell():
@@ -23,13 +24,17 @@ def create_game_of_life_with_all_dead_cell():
 
 
 def test_given_living_cell_with_0_live_nieghbors_it_dies():
-    # create a game of life
     game = create_game_of_life_with_all_dead_cell()
-    # set (1,1) cell to alive
     game.set_cell_alive(1,1)
     assert game.get_status(0, 0) == DEAD
     assert game.get_status(1, 1) == ALIVE
-    # move to the next point in time
     game.move_to_next_time()
-    # the cell at 1,1 is now dead
     assert game.get_status(1,1) == DEAD
+
+
+def test_0_0_is_alive():
+    game = create_game_of_life_with_all_dead_cell()
+    game.set_cell_alive(0,0)
+    assert game.get_status(0, 0) == ALIVE
+
+

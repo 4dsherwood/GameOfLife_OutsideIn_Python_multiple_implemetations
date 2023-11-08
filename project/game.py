8e7d7dbc1381dce4d6165497c1b9bfe53e4eb2b9
4dsherwood <4dsherwood@users.noreply.github.com>
2023-11-08 15:43:23 -0700

    fixed  error for closures

--- project/game.py
+++ project/game.py
@@ -3,29 +3,31 @@
 class Game:
     def __init__(self):
         def dead_board(x,y):
+            print("DEAD")
             return DEAD
         #self.cells = lambda x,y:DEAD
         self.cells = dead_board
     def set_cell_alive(self, x, y):
         old_board = self.cells # takes the lambda function
         def new_board(x1,y1): # closure (aka inner funciton)
+            print(f"inner closure for ({x},{y}) called with ({x1},{y1})")
             if x == x1 and y == y1:
                 return ALIVE
             else:
-                return old_board(x,y)
+                return old_board(x1,y1)
         self.cells = new_board
 
     def move_to_next_time(self):
-        game = Game()
         previous = self.cells
         def next_board(x,y):
-            count = previous(x -1, y-1) + previous(x-1, y) + previous(x-1, y+1) \
+            count = previous(x-1, y-1) + previous(x-1, y) + previous(x-1, y+1) \
                 + previous(x,y-1) + previous(x,y+1) \
                 + previous(x+1, y-1) + previous(x+1,y) + previous(x+1, y+1)
             if count == 3 or count == 2 and previous(x,y) == ALIVE:
                 return ALIVE
             else:
                 return DEAD
+        game = Game()
         game.cells = next_board
         return game
 
--- tests/test_samples.py
+++ tests/test_samples.py
@@ -7,16 +7,30 @@ def create_game_of_life_with_all_dead_cell():
 
 def test_given_living_cell_with_0_live_nieghbors_it_dies():
     game = create_game_of_life_with_all_dead_cell()
-    game.set_cell_alive(1,1)
+    game.set_cell_alive(1, 1)
     assert game.get_status(0, 0) == DEAD
     assert game.get_status(1, 1) == ALIVE
     game = game.move_to_next_time()
-    assert game.get_status(1,1) == DEAD
+    assert game.get_status(1, 1) == DEAD
 
 
 def test_0_0_is_alive():
     game = create_game_of_life_with_all_dead_cell()
-    game.set_cell_alive(0,0)
+    game.set_cell_alive(0, 0)
     assert game.get_status(0, 0) == ALIVE
 
 
+def test_blinker():
+    game = create_game_of_life_with_all_dead_cell()
+    game.set_cell_alive(2, 1)
+    game.set_cell_alive(3, 1)
+    game.set_cell_alive(4, 1)
+    game.set_cell_alive(1, 2)
+    game.set_cell_alive(2, 2)
+    game.set_cell_alive(3, 2)
+    assert game.get_status(2, 2) == ALIVE
+    assert game.get_status(5, 6) == DEAD
+    step1 = game.move_to_next_time()
+    step2 = step1.move_to_next_time()
+    assert step1.get_status(1, 2) == ALIVE
+    # assert step2.get_status(2,1) == ALIVE

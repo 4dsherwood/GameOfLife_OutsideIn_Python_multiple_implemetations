ac38913a68e903ae752219a6788f6935e0521174
4dsherwood <4dsherwood@users.noreply.github.com>
2023-11-08 14:44:08 -0700

    convert lamda to inner class

--- project/game.py
+++ project/game.py
@@ -2,21 +2,25 @@
 
 class Game:
     def __init__(self):
-        # list cells
-        # list.get((1,1))
-        # lookup of cells
-        # lookup.get(1,1)
-        self.cells = ""
+        def dead_board(x,y):
+            return DEAD
+        #self.cells = lambda x,y:DEAD
+        self.cells = dead_board
     def set_cell_alive(self, x, y):
-        self.cells += f"({x},{y})"
+        old_board = self.cells # takes the lambda function
+        def new_board(x1,y1): # closure (aka inner funciton)
+            if x == x1 and y == y1:
+                return ALIVE
+            else:
+                return old_board(x,y)
+        self.cells = new_board
 
     def move_to_next_time(self):
         return Game()
 
-    def get_status(self, x, y):
-        key = f"({x},{y})"
-        return ALIVE if key in self.cells else DEAD
+    def get_status(self, x, y)-> int:
+        return self.cells(x,y)
 
 
-DEAD = 0
-ALIVE = 1
+DEAD: int = 0
+ALIVE: int = 1

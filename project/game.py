7e6d19e236e248941fd38ff7b2d0c3a3ab48c441
4dsherwood <4dsherwood@users.noreply.github.com>
2023-11-08 14:55:14 -0700

    Full implemetation using closures (aka inner class) similar to lamda but not a lamda

--- project/game.py
+++ project/game.py
@@ -16,7 +16,18 @@ class Game:
         self.cells = new_board
 
     def move_to_next_time(self):
-        return Game()
+        game = Game()
+        previous = self.cells
+        def next_board(x,y):
+            count = previous(x -1, y-1) + previous(x-1, y) + previous(x-1, y+1) \
+                + previous(x,y-1) + previous(x,y+1) \
+                + previous(x+1, y-1) + previous(x+1,y) + previous(x+1, y+1)
+            if count == 3 or count == 2 and previous(x,y) == ALIVE:
+                return ALIVE
+            else:
+                return DEAD
+        game.cells = next_board
+        return game
 
     def get_status(self, x, y)-> int:
         return self.cells(x,y)
